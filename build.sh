6b020ac160b641045e997ed14da79cf60e286d3d
Mike Leonard <mike.r.leonard@gmail.com>
2014-02-06 01:18:12 +1100

    Modified the build script to sort out the images for me.

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-zip FormAlert.zip LICENSE css/ js/ options.html README.md images/ manifest.json
+# Copy the images to the right locations fo the Chrome extension (as specified in manifest.json).
+mkdir images
+cp promotional_images/icon.png images/fa_icon_128.png
+
+# Create the archive for uploading.
+zip FormAlert.zip LICENSE css/* js/* options.html README.md images/* manifest.json
+
+# Tidy up the images directory.
+rm -rf images
