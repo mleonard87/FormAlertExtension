bb26fb6dfae17f1b90d2ea2fc93bfafda5044320
Mike Leonard <mike.r.leonard@gmail.com>
2014-02-06 00:02:19 +1100

    Fixed the issue where settings changes were not reflected immediately. (Github issue #2)

--- js/fa_background.js
+++ js/fa_background.js
@@ -51,12 +51,18 @@ if (window['is_options_page'] === undefined) {
     function(request, sender, sendResponse) {
       if (request.greeting == 'get_vars')
         sendResponse({
-          'display_callout': display_callout,
-          'display_icon': display_icon,
-          'border_color': border_color,
-          'border_width': border_width,
-          'border_style': border_style
+          'display_callout': (localStorage['display_callout'] != undefined) ? parseBoolean(localStorage['display_callout']) : default_display_callout,
+          'display_icon': (localStorage['display_icon'] != undefined) ? parseBoolean(localStorage['display_icon']) : default_display_icon,
+          'border_color': (localStorage['border_color'] != undefined) ? localStorage['border_color'] : default_border_color,
+          'border_width': (localStorage['border_width'] != undefined) ? localStorage['border_width'] : default_border_width,
+          'border_style': (localStorage['border_style'] != undefined) ? localStorage['border_style'] : default_border_style
         });
     }
   );
 }
+
+
+
+
+
+
